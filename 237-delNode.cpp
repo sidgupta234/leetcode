2aaa41df319d6cd6d6d66bda1be944727329fd07
Siddharth Gupta <siddharthgupta234@gmail.com>
2016-06-16 18:12:22 +0530

    Create 237-delNode.cpp

---
+++ 237-delNode.cpp
@@ -0,0 +1,17 @@
+https://leetcode.com/problems/delete-node-in-a-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val=node->next->val; // copying the next element to the present element.
+        node->next=node->next->next; // skipping the next element.
+    }
+};
